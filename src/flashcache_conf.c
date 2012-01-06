3aa9564c30e133aa1475040714a0deb34f5b6a41
Pavel Snajdr <psnajdr@redhat.com>
2012-01-07 00:27:11 +0100

    Parentheses for better readability

--- src/flashcache_conf.c
+++ src/flashcache_conf.c
@@ -1646,7 +1646,7 @@ flashcache_init(void)
 		return r;
 	}
 #else /* LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,22) */
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,0,0) || defined(RHEL_RELEASE_CODE) && (RHEL_RELEASE_CODE >= 1538)
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(3,0,0)) || (defined(RHEL_RELEASE_CODE) && (RHEL_RELEASE_CODE >= 1538))
 	flashcache_io_client = dm_io_client_create();
 #else
 	flashcache_io_client = dm_io_client_create(FLASHCACHE_COPY_PAGES);
@@ -1658,7 +1658,7 @@ flashcache_init(void)
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,26)
 	r = kcopyd_client_create(FLASHCACHE_COPY_PAGES, &flashcache_kcp_client);
-#elif LINUX_VERSION_CODE >= KERNEL_VERSION(3,0,0) || defined(RHEL_RELEASE_CODE) && (RHEL_RELEASE_CODE >= 1538)
+#elif (LINUX_VERSION_CODE >= KERNEL_VERSION(3,0,0)) || (defined(RHEL_RELEASE_CODE) && (RHEL_RELEASE_CODE >= 1538))
 	flashcache_kcp_client = dm_kcopyd_client_create();
 	if ((r = IS_ERR(flashcache_kcp_client))) {
 		r = PTR_ERR(flashcache_kcp_client);
